72afcef7ba49f7974e6e43d88727c5b64e9d13bb
puspa bhattarai <bhattaraipuspa04@gmail.com>
2025-11-19 12:56:29 +0545

    github

--- w3.java
+++ w3.java
@@ -6,10 +6,20 @@
  * @version (a version number or a date)
  */
 public class w3
+
 {
+    int age; //instance variable
+    static int agr; //static variable
+    
   public static void main(String[] args){
-      
+   int city; //local variable
+   w3 w=new w3();
+   
   System.out.println ("github");
+  
+  System.out.println(agr);
+  
+  
 
     }
 }
