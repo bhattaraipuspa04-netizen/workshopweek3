9f868f19650404e139f61808edd6d48f71429d0e
puspa bhattarai <bhattaraipuspa04@gmail.com>
2025-11-19 12:44:02 +0545

    github

--- w3.java
+++ w3.java
@@ -7,27 +7,9 @@
  */
 public class w3
 {
-    // instance variables - replace the example below with your own
-    private int x;
+  public static void main(String[] args){
+      
+  System.out.println ("github");
 
-    /**
-     * Constructor for objects of class w3
-     */
-    public w3()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
     }
 }
